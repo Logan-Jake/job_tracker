8af95e4249a8b4cd3e0ba72edc72c02ac22834a6
jakel <jakelogan35@gmail.com>
2026-07-28 08:53:14 +0100

    updated db connection type in config.py be correct for docker application.

--- db/config.py
+++ db/config.py
@@ -1,22 +1,18 @@
-from configparser import ConfigParser
-from pathlib import Path
+import os
+from urllib.parse import urlparse
 # this file loads the db connection details from database.ini
 
 
 def load_config(filename='database.ini', section='postgresql'):
-    filepath = Path(__file__).resolve().parent / filename
-    parser = ConfigParser()
-    parser.read(filepath)
-
-    config = {}
-    if parser.has_section(section):
-        params = parser.items(section)
-        for param in params:
-            config[param[0]] = param[1]
-    else:
-        raise Exception('Section {0} not found in the {1} file'.format(section, filename))
-
-    return config
+    url = os.environ['DATABASE_URL']
+    parsed = urlparse(url)
+    return {
+        "host": parsed.hostname,
+        "port": parsed.port,
+        "dbname": parsed.path[1:],   # strip the leading slash
+        "user": parsed.username,
+        "password": parsed.password,
+    }
 
 
 if __name__ == '__main__':
